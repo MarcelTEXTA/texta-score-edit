859bf59959fd47d0d1ef4cfb1d60cd4e1e6f4a3a
MarcelTEXTA <ethanmacaluso265@gmail.com>
2026-07-21 22:01:22 +0200

    add files UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import "./styles/global.css";
+import "./styles/layout.css";
+
+import Sidebar from "./components/Sidebar/Sidebar";
+import Toolbar from "./components/Toolbar/Toolbar";
+import ScoreCanvas from "./components/Canvas/ScoreCanvas";
 
 export default function App() {
-
   return (
-    <>
-      <h1>Welcome to MarcelTEXTA app</h1>
-      <p>Hello world React !</p>
-    </>
-  )
+    <div className="container">
+      <Toolbar />
+      <div className="main">
+        <Sidebar />
+        <ScoreCanvas />
+      </div>
+    </div>
+  );
 }
--- src/components/TitleBar/TitleBar.tsx
+++
@@ -1,9 +0,0 @@
-import './titlebar.css'
-
-export default function titlebar(){
-  return(
-		<div className='title-bar'>
-			<p>Title bar</p>
-		</div>
-  );
-}
